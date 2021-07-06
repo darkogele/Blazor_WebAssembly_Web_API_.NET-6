b002537fbd088ae600c45951381f77e0adf75895
Darko Gelevski <Darkogele@hotmail.com>
2021-07-06 15:35:02 +0200

    Add project files.

---
+++ SuperHeroDb/Client/Services/ISuperHeroService.cs
@@ -0,0 +1,20 @@
+﻿using SuperHeroDb.Shared;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SuperHeroDb.Client.Services
+{
+    public interface ISuperHeroService
+    {
+        event Action OnChange;
+        List<Comic> Comics { get; set; }
+        List<SuperHero> Heroes { get; set; }
+        Task<List<SuperHero>> GetSuperHeroes();
+        Task GetComics();
+        Task<SuperHero> GetSuperHero(int id);
+        Task<List<SuperHero>> CreateSuperHero(SuperHero superHero);
+        Task<List<SuperHero>> UpdateSuperHero(SuperHero superHero);
+        Task<List<SuperHero>> DeleteSuperHero(int id);      
+    }
+}
---
+++ SuperHeroDb/Client/Services/SuperHeroService.cs
@@ -0,0 +1,71 @@
+﻿using SuperHeroDb.Shared;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace SuperHeroDb.Client.Services
+{
+    public class SuperHeroService : ISuperHeroService
+    {
+        private readonly HttpClient _httpClient;
+
+        public SuperHeroService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public List<Comic> Comics { get; set; } = new();
+        public List<SuperHero> Heroes { get; set; } = new();
+
+        public event Action OnChange;
+
+        public async Task<List<SuperHero>> CreateSuperHero(SuperHero superHero)
+        {
+            var result = await _httpClient.PostAsJsonAsync($"api/superhero", superHero);
+
+            Heroes = await result.Content.ReadFromJsonAsync<List<SuperHero>>();
+            OnChange.Invoke();
+
+            return Heroes;
+        }
+
+        public async Task<List<SuperHero>> DeleteSuperHero(int id)
+        {
+            var result = await _httpClient.DeleteAsync($"api/superhero/{id}");
+            
+            Heroes = await result.Content.ReadFromJsonAsync<List<SuperHero>>();
+            OnChange.Invoke();
+
+            return Heroes;
+        }
+
+        public async Task GetComics()
+        {
+            Comics = await _httpClient.GetFromJsonAsync<List<Comic>>("api/superhero/comics");
+        }
+
+        public async Task<SuperHero> GetSuperHero(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<SuperHero>($"api/superhero/{id}");
+        }
+
+        public async Task<List<SuperHero>> GetSuperHeroes()
+        {
+            Heroes = await _httpClient.GetFromJsonAsync<List<SuperHero>>("api/superhero");
+
+            return Heroes;
+        }
+
+        public async Task<List<SuperHero>> UpdateSuperHero(SuperHero superHero)
+        {
+            var result = await _httpClient.PutAsJsonAsync($"api/superhero", superHero);
+
+            Heroes = await result.Content.ReadFromJsonAsync<List<SuperHero>>();
+            OnChange.Invoke();
+
+            return Heroes;
+        }
+    }
+}
